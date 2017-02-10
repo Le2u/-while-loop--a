c0f68d14e16fc6bd8f4fa1ba921166d2120a19ee
Le2u <lijiacheng@lijiachengs-MacBook-Pro.local>
2017-02-10 17:32:02 +0800

    显示数字位数

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    int a ;
+    int n = 0 ;
+    
+    scanf("%d",&a);
+    n++;
+    a /= 10;
+    while ( a > 0 ) {
+        n++ ;
+        a /= 10 ;
+    }
+    
+    printf("%d\n",n);
+    return 0;
+}
